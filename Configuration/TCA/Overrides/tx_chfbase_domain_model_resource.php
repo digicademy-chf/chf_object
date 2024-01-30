76a0b539f86b38888daea67c2f0746504feb9609
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-01-30 16:15:35 +0100

    Add ObjectResource TCA

---
+++ Configuration/TCA/Overrides/tx_chfbase_domain_model_resource.php
@@ -0,0 +1,85 @@
+<?php
+defined('TYPO3') or die();
+
+# This file is part of the extension CHF Object for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+/**
+ * ObjectResource and its properties
+ * 
+ * Extension of a database table and its editing interface in the
+ * TYPO3 backend. This also serves as the basis for the Extbase
+ * domain model. For more information on TCA and its options see
+ * https://docs.typo3.org/m/typo3/reference-tca/main/en-us/.
+ */
+
+// Add select item 'objectResource'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem('tx_chfbase_domain_model_resource', 'type',
+    [
+        'label' => 'LLL:EXT:chf_object/Resources/Private/Language/locallang.xlf:object.objectResource.type.objectResource',
+        'value' => 'objectResource',
+    ]
+);
+
+// Add columns 'allObjectGroups' and 'allSingleObjects'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_chfbase_domain_model_resource',
+    [
+        'allObjectGroups' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_object/Resources/Private/Language/locallang.xlf:object.objectResource.allObjectGroups',
+            'description' => 'LLL:EXT:chf_object/Resources/Private/Language/locallang.xlf:object.objectResource.allObjectGroups.description',
+            'config' => [
+                'type' => 'inline',
+                'foreign_table' => 'tx_chfobject_domain_model_object_group',
+                'foreign_field' => 'parentResource',
+                'foreign_sortby' => 'sorting',
+                'appearance' => [
+                    'collapseAll' => true,
+                    'expandSingle' => true,
+                    'newRecordLinkAddTitle' => true,
+                    'levelLinksPosition' => 'top',
+                    'useSortable' => false,
+                    'showPossibleLocalizationRecords' => true,
+                    'showAllLocalizationLink' => true,
+                    'showSynchronizationLink' => true,
+                ],
+            ],
+        ],
+        'allSingleObjects' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_object/Resources/Private/Language/locallang.xlf:object.objectResource.allSingleObjects',
+            'description' => 'LLL:EXT:chf_object/Resources/Private/Language/locallang.xlf:object.objectResource.allSingleObjects.description',
+            'config' => [
+                'type' => 'inline',
+                'foreign_table' => 'tx_chfobject_domain_model_single_object',
+                'foreign_field' => 'parentResource',
+                'foreign_sortby' => 'sorting',
+                'appearance' => [
+                    'collapseAll' => true,
+                    'expandSingle' => true,
+                    'newRecordLinkAddTitle' => true,
+                    'levelLinksPosition' => 'top',
+                    'useSortable' => false,
+                    'showPossibleLocalizationRecords' => true,
+                    'showAllLocalizationLink' => true,
+                    'showSynchronizationLink' => true,
+                ],
+            ],
+        ],
+    ]
+);
+
+// Add type 'objectResource' and its 'showitem' list
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+   'tx_chfbase_domain_model_resource',
+   'hiddenUuid,typeUri,titleLangCode,glossary,description,sameAs,
+   --div--;LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.editorial,authorshipRelation,licenceRelation,publicationDateRevisionNumberRevisionDate,editorialNote,
+   --div--;LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.content,allAgents,allFileCollections,allLocations,allPeriods,allRelations,allTags,allObjectGroups,allSingleObjects,
+   --div--;LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.import,importOrigin,importState,'
+   'objectResource'
+);
