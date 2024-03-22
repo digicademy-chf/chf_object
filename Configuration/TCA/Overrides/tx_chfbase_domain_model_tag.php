52da6f542609260d2a4f7ba283ef863ea5a7c948
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-03-22 14:35:14 +0100

    Add LabelTag and Extent overwrites

---
+++ Configuration/TCA/Overrides/tx_chfbase_domain_model_tag.php
@@ -0,0 +1,87 @@
+<?php
+declare(strict_types=1);
+
+# This file is part of the extension CHF Bib for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+defined('TYPO3') or die();
+
+/**
+ * LabelTag and its properties
+ * 
+ * Extension of a database table and its editing interface in the
+ * TYPO3 backend. This also serves as the basis for the Extbase
+ * domain model. For more information on TCA and its options see
+ * https://docs.typo3.org/m/typo3/reference-tca/main/en-us/.
+ */
+
+// Add column 'asLabelOfSingleObject'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_chfbase_domain_model_tag',
+    [
+        'asLabelOfSingleObject' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_object/Resources/Private/Language/locallang.xlf:object.labelTag.asLabelOfSingleObject',
+            'description' => 'LLL:EXT:chf_object/Resources/Private/Language/locallang.xlf:object.labelTag.asLabelOfSingleObject.description',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectMultipleSideBySide',
+                'foreign_table' => 'tx_chfobject_domain_model_single_object',
+                'foreign_table_where' => 'AND {#tx_chfobject_domain_model_single_object}.{#pid}=###CURRENT_PID###',
+                'MM' => 'tx_chfobject_domain_model_single_object_tag_label_mm',
+                'MM_opposite_field' => 'label',
+                'size' => 5,
+                'autoSizeMax' => 10,
+                'fieldControl' => [
+                    'editPopup' => [
+                        'disabled' => false,
+                    ],
+                    'addRecord' => [
+                        'disabled' => false,
+                    ],
+                    'listModule' => [
+                        'disabled' => false,
+                    ],
+                ],
+                'readOnly' => true,
+            ],
+        ],
+    ]
+);
+
+// Add column 'asLabelOfObjectGroup'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_chfbase_domain_model_tag',
+    [
+        'asLabelOfObjectGroup' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_object/Resources/Private/Language/locallang.xlf:object.labelTag.asLabelOfObjectGroup',
+            'description' => 'LLL:EXT:chf_object/Resources/Private/Language/locallang.xlf:object.labelTag.asLabelOfObjectGroup.description',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectMultipleSideBySide',
+                'foreign_table' => 'tx_chfobject_domain_model_object_group',
+                'foreign_table_where' => 'AND {#tx_chfobject_domain_model_object_group}.{#pid}=###CURRENT_PID###',
+                'MM' => 'tx_chfobject_domain_model_object_group_tag_label_mm',
+                'MM_opposite_field' => 'label',
+                'size' => 5,
+                'autoSizeMax' => 10,
+                'fieldControl' => [
+                    'editPopup' => [
+                        'disabled' => false,
+                    ],
+                    'addRecord' => [
+                        'disabled' => false,
+                    ],
+                    'listModule' => [
+                        'disabled' => false,
+                    ],
+                ],
+                'readOnly' => true,
+            ],
+        ],
+    ]
+);
