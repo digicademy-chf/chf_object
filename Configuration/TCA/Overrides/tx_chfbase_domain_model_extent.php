e954e99ccdc857ec067a70f04ad714073400259d
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-03-24 09:22:49 +0100

    Fix 2 file headers

--- Configuration/TCA/Overrides/tx_chfbase_domain_model_extent.php
+++ Configuration/TCA/Overrides/tx_chfbase_domain_model_extent.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-# This file is part of the extension CHF Pub for TYPO3.
+# This file is part of the extension CHF Object for TYPO3.
 #
 # For the full copyright and license information, please read the
 # LICENSE.txt file that was distributed with this source code.
