79b63a49aee4af1662d3f64fb88f5622ffe1498c
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-06-10 15:37:12 +0200

    Fix PHP statements

--- Classes/Controller/ObjectGroupController.php
+++ Classes/Controller/ObjectGroupController.php
@@ -1,5 +1,4 @@
 <?php
-defined('TYPO3') or die();
 declare(strict_types=1);
 
 # This file is part of the extension CHF Object for TYPO3.
@@ -14,6 +13,8 @@ use Psr\Http\Message\ResponseInterface;
 use Digicademy\CHFObject\Domain\Model\ObjectGroup;
 use Digicademy\CHFObject\Domain\Repository\ObjectGroupRepository;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+defined('TYPO3') or die();
 
 /**
  * Controller for ObjectGroup
